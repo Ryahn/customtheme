04e16c4f4dad6d3cd83b651019f5cbb7430bfde2
Ryahn <ryancarr10@gmail.com>
2019-05-19 23:58:39 -0400

    Added background rotation logic

--- theme.meta.js
+++ theme.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://github.com/Ryahn/customtheme
-// @version      0.1.10
+// @version      0.1.11
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
--- theme.user.js
+++ theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://upload.multizone.pw/*
-// @version      0.1.10
+// @version      0.1.11
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
@@ -15,7 +15,10 @@
 // ==/UserScript==
 
 
+const images = ['https://public.b-cdn.net/static/101914.png', 'https://public.b-cdn.net/static/101918.png'];
 //Get custom theme
 let newCSS = GM_getResourceText("customCSS");
 //Load theme
 GM_addStyle(newCSS);
+
+$('body').css({'background': 'linear-gradient(0deg, rgba(0, 0, 0, .56), rgba(0, 0, 0, .56)), url(' + images[Math.floor(Math.random() * images.length)] + ')'});
