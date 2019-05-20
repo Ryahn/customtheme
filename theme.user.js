ce1710015d5508abbe7a4ab98c9dbd0300209351
SventraPopizz <30782292+SventraPopizz@users.noreply.github.com>
2019-05-20 08:56:51 +0200

    Added transparency

--- theme.user.js
+++ theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://upload.multizone.pw/*
-// @version      0.2.6
+// @version      0.2.7
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
