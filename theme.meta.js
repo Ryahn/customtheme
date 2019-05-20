b31628646f567e7d17a271899899612a6f86d95d
SventraPopizz <30782292+SventraPopizz@users.noreply.github.com>
2019-05-20 08:30:58 +0200

    Fixed default wallpaper

--- theme.meta.js
+++ theme.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://github.com/Ryahn/customtheme
-// @version      0.2.4
+// @version      0.2.5
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
