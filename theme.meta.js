5fd1cb8ab136e358e85140a78d4425ddd037499f
Ryahn <ryancarr10@gmail.com>
2019-05-19 23:12:57 -0400

    Test3

--- theme.meta.js
+++ theme.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://github.com/Ryahn/customtheme
-// @version      0.3
+// @version      0.4
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
@@ -9,6 +9,5 @@
 // @updateURL    https://github.com/Ryahn/customtheme/raw/master/theme.meta.js
 // @downloadURL  https://github.com/Ryahn/customtheme/raw/master/theme.user.js
 // @supportURL   https://github.com/Ryahn/customtheme/issues
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @grant        none
 // ==/UserScript==
--- theme.user.js
+++ theme.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://upload.multizone.pw/*
-// @version      0.3
+// @version      0.4
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
 // @match        *://*.multizone.pw/*
 // @updateURL    https://github.com/Ryahn/customtheme/raw/master/theme.meta.js
 // @downloadURL  https://github.com/Ryahn/customtheme/raw/master/theme.user.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @grant        none
 // ==/UserScript==
 
