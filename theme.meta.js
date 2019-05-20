53bf3d3e39b3debcc2dccdc66b0513c199bb74e1
SventraPopizz <30782292+SventraPopizz@users.noreply.github.com>
2019-05-20 08:56:38 +0200

    Added transparency

--- theme.meta.js
+++ theme.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://github.com/Ryahn/customtheme
-// @version      0.2.6
+// @version      0.2.7
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
