abedb982ccbc2143e4afa1e6ca20c45a772b4b6c
Ryahn <ryancarr10@gmail.com>
2019-05-19 23:42:49 -0400

    Fixing theme

--- theme.meta.js
+++ theme.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://github.com/Ryahn/customtheme
-// @version      0.1.8
+// @version      0.1.9
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
--- theme.user.js
+++ theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://upload.multizone.pw/*
-// @version      0.1.8
+// @version      0.1.9
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
@@ -14,20 +14,8 @@
 // @grant        GM_getResourceText
 // ==/UserScript==
 
-(function() {
-    'use strict';
 
-    //Get custom theme
-     let newCSS = GM_getResourceText("customCSS");
-     //Load theme
-     GM_addStyle(newCSS);
-    //  let $head = $('head');
-    //  let $last = $head.find('link[rel="stylesheet"]:last');
-    //  let link = '<link rel="stylesheet" href="https://raw.githubusercontent.com/Ryahn/customtheme/master/theme.css" type="text/css">';
-
-    //  if ($last.length) {
-    //      $last.after(link);
-    //  } else {
-    //      $head.append(link);
-    //  }
-})(); 
+//Get custom theme
+let newCSS = GM_getResourceText("customCSS");
+//Load theme
+GM_addStyle(newCSS);
