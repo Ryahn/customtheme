abfb5f728c767bc190f5da731820c17cfa3b652a
Ryahn <ryancarr10@gmail.com>
2019-05-19 23:08:50 -0400

    Test

---
+++ theme.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Multi Theme
+// @namespace    https://github.com/Ryahn/customtheme
+// @version      0.2
+// @description  Custom theme
+// @author       Ryahn
+// @contributor  Ryahn
+// @match        *://*.multizone.pw/*
+// @updateURL    https://github.com/Ryahn/customtheme/raw/master/theme.meta.js
+// @downloadURL  https://github.com/Ryahn/customtheme/raw/master/theme.user.js
+// @supportURL   https://github.com/Ryahn/customtheme/issues
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
---
+++ theme.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Multi Theme
+// @namespace    https://upload.multizone.pw/*
+// @version      0.2
+// @description  Custom theme
+// @author       Ryahn
+// @contributor  Ryahn
+// @match        *://*.multizone.pw/*
+// @updateURL    https://github.com/Ryahn/customtheme/raw/master/theme.meta.js
+// @downloadURL  https://github.com/Ryahn/customtheme/raw/master/theme.user.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('Test');
+})();
