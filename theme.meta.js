a5b394c1f406f0d6dfbb7f1036ec633aa277d088
Ryahn <ryancarr10@gmail.com>
2019-05-19 23:40:31 -0400

    Added GM function

--- theme.meta.js
+++ theme.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://github.com/Ryahn/customtheme
-// @version      0.1.7
+// @version      0.1.8
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
--- theme.user.js
+++ theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Theme
 // @namespace    https://upload.multizone.pw/*
-// @version      0.1.7
+// @version      0.1.8
 // @description  Custom theme
 // @author       Ryahn
 // @contributor  Ryahn
@@ -9,20 +9,25 @@
 // @updateURL    https://raw.githubusercontent.com/Ryahn/customtheme/master/theme.meta.js
 // @downloadURL  https://raw.githubusercontent.com/Ryahn/customtheme/master/theme.user.js
 // @supportURL   https://github.com/Ryahn/customtheme/issues
-// @grant        none
+// @require      customCSS https://raw.githubusercontent.com/Ryahn/customtheme/master/theme.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    //Test
-     let $head = $('head');
-     let $last = $head.find('link[rel="stylesheet"]:last');
-     let link = '<link rel="stylesheet" href="https://raw.githubusercontent.com/Ryahn/customtheme/master/theme.css" type="text/css">';
+    //Get custom theme
+     let newCSS = GM_getResourceText("customCSS");
+     //Load theme
+     GM_addStyle(newCSS);
+    //  let $head = $('head');
+    //  let $last = $head.find('link[rel="stylesheet"]:last');
+    //  let link = '<link rel="stylesheet" href="https://raw.githubusercontent.com/Ryahn/customtheme/master/theme.css" type="text/css">';
 
-     if ($last.length) {
-         $last.after(link);
-     } else {
-         $head.append(link);
-     }
+    //  if ($last.length) {
+    //      $last.after(link);
+    //  } else {
+    //      $head.append(link);
+    //  }
 })(); 
